0956382581daaa7bc385ff4133f39129692976a8
matt <matt@491678-web-db1.strength.org>
2013-07-10 20:17:57 -0500

    initial version of script

---
+++ migrate.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+
+import os
+from optparse import OptionParser
+from migration_data import *
+
+parser = OptionParser()
+
+parser.add_option('-s', action="store", dest="backup_server",
+    help="type string", default="all")
+
+options, args = parser.parse_args()
+
+
+def run_file_migration(name):
+   
+    if name == 'all':
+        for migration in FILE_MIGRATIONS:
+            excludes = ''
+            for exclude in migration['exclude']:
+                excludes = '%s --exclude=%s' % (excludes, exclude)
+            rsync_command = 'rsync -rlv %s %s@%s:%s %s' % (excludes, migration['user'], migration['host'], migration['remote_dir'], migration['local_dir'])
+            os.system(rsync_command)
+
+
+if __name__ == '__main__':
+    run_file_migration(options.backup_server)
+
---
+++ migration_data.example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Don't change these
+import os
+
+FILE_MIGRATIONS = [
+    {
+    'host': 'mysite.org',
+	'user': 'myuser',
+	'remote_dir': '/path/to/remote/files/',
+	'local_dir': '/local/destination/path/',
+	'exclude': ['example1', 'example']
+    },
+]
